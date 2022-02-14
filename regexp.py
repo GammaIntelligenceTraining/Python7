09462d6d0e22643eeb67b3fdcc10fc0c3379580c
mrartful <roman.kutselepa@gmail.com>
2022-02-14 12:06:41 +0200

    Reg exp

---
+++ regexp.py
@@ -0,0 +1,23 @@
+text_to_search = '''
+abcdefghijklmnopqurtuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+1234567890
+Ha HaHa HaHaH
+MetaCharacters (Need to be escaped):
+. ^ $ * + ? { } [ ] \ | ( )
+example.com
+321-555-4321
+123.555.1234
+123*555*1234
+800-555-1234
+900-555-1234
+Mr. Jones
+Mr Smith
+Ms Davis
+Mrs. Robinson
+Mr. T
+abc
+
+ball mall hall wall tall
+'''
+sentence = 'Start a sentence and then bring it to an end Start'
